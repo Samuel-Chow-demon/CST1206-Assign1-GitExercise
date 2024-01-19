d54c30b6a29927108d1ba9ef7faebb6c13846e3f
-v <demondemontcchow@gmail.com>
2024-01-19 15:17:29 -0800

    (1) Add Section - FAQ (2) Add script to hover and expand the FAQ content

--- script/script.js
+++ script/script.js
@@ -1,6 +1,10 @@
+const root = document.documentElement;
+const colorQuestionBkgd = window.getComputedStyle(root).getPropertyValue("--question-background-color");
 
 const arrTabButtons = document.querySelectorAll(".tab-button");
 const arrSectionContents = document.querySelectorAll(".tab-content");
+const arrFAQQuestions = document.querySelectorAll(".faq-question");
+
 for (let tabButton of arrTabButtons)
 {
   tabButton.addEventListener('click', (event)=>{
@@ -22,4 +26,32 @@ for (let tabButton of arrTabButtons)
     // The current pressed button set to active color
     event.target.classList.add("active");
   });
+}
+
+for (let question of arrFAQQuestions)
+{
+  question.addEventListener("click", (event)=>{
+    // Since the last had a icon, need to get back the li item object
+    const currentListItem = event.target.closest("li");
+    const answer = currentListItem.nextElementSibling;
+
+    if (answer.classList.contains("faq-answer-expand"))
+    {
+      answer.classList.remove("faq-answer-expand");
+    }
+    else
+    {
+      answer.classList.add("faq-answer-expand");
+    }
+  });
+
+  question.addEventListener("mouseover", (event)=>{
+    const currentListItem = event.target.closest("li");
+    currentListItem.style.background = "#e2b68d";
+  })
+
+  question.addEventListener("mouseout", (event)=>{
+    const currentListItem = event.target.closest("li");
+    currentListItem.style.background = colorQuestionBkgd;
+  })
 }
